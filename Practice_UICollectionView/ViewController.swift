64d244fa235e324023c00cccc8f9529ddf880c46
abrahamshenghur <ashenghur@gmail.com>
2021-07-26 10:33:58 -0700

    Display collection view in a grid layout using blank cells

--- Practice_UICollectionView/ViewController.swift
+++ Practice_UICollectionView/ViewController.swift
@@ -10,35 +10,53 @@ import UIKit
 
 class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
-    var collectionView: UICollectionView!
-    var cellId = "Cell"
+    private let cellReuseIdentifier = "collectionCell"
+    
+    let leftAndRightPaddings: CGFloat = 80.0
+    let numberOfItemsPerRow: CGFloat = 7.0
+    let screenSize: CGRect = UIScreen.main.bounds
+    
+    var items = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Create an instance of UICollectionViewFlowLayout since you cant
-        // Initialize UICollectionView without a layout
-        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
-        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-        layout.itemSize = CGSize(width: view.frame.width, height: 700)
-        
-        collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
-        collectionView.dataSource = self
-        collectionView.delegate = self
-        collectionView.register(Cell.self, forCellWithReuseIdentifier: cellId)
+        let flowLayout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: flowLayout)
+        collectionView.register(Cell.self, forCellWithReuseIdentifier: cellReuseIdentifier)
         collectionView.showsVerticalScrollIndicator = false
         collectionView.backgroundColor = UIColor.white
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
         self.view.addSubview(collectionView)
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        
+        return items.count
     }
     
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! Cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellReuseIdentifier, for: indexPath) as! Cell
+        cell.backgroundColor = UIColor.green
+        
         return cell
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize
+    {
+        let width = (screenSize.width-leftAndRightPaddings)/numberOfItemsPerRow
+        
+        return CGSize(width: width, height: width)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets
+    {
+        return UIEdgeInsets(top: 20, left: 8, bottom: 5, right: 8)
     }
 }
 
