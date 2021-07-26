9a8d8ab119164959b86363c65b9b01a40e49247d
abrahamshenghur <ashenghur@gmail.com>
2021-07-26 10:25:23 -0700

    Initial commit

---
+++ Practice_UICollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Practice_UICollectionView
+//
+//  Created by Abraham on 7/26/21.
+//  Copyright © 2021 Abraham Shenghur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
